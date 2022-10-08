322cbaa25002790387bf6dcc905a2b94b91eef15
i0bs <41456914+i0bs@users.noreply.github.com>
2022-10-08 18:54:31 -0400

    chore: copyright notice on entry

--- MiG-25RBT/entry.lua
+++ MiG-25RBT/entry.lua
@@ -1,5 +1,10 @@
 --[[
+	GNU AFFERO GENERAL PUBLIC LICENSE
+	Version 3, 19 November 2007
 
+	Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+	Everyone is permitted to copy and distribute verbatim copies
+	of this license document, but changing it is not allowed.
 ]]--
 
 local self_ID = "MiG-25RBT"
