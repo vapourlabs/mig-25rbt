241b6b723c64d4c46ace5c16ca05cb613d4b098f
i0bs <41456914+i0bs@users.noreply.github.com>
2022-10-08 20:56:26 -0400

    fix: correct string error in entry info

--- MiG-25RBT/entry.lua
+++ MiG-25RBT/entry.lua
@@ -9,8 +9,8 @@
 
 local self_ID = "MiG-25RBT"
 local systems_path = current_mod_path .. "/Cockpit/Scripts"
-local themes_path = current_mod_path .. "/Theme/ME/"
-local model_path = current_mod_path .. "/Shapes"
+-- local themes_path = current_mod_path .. "/Theme"
+-- local model_path = current_mod_path .. "/Shapes"
 -- local liveries_path = current_mod_path .. "/Liveries"
 
 declare_plugin(self_ID .. " Foxbat by Vapour Labs", {
@@ -24,11 +24,17 @@ declare_plugin(self_ID .. " Foxbat by Vapour Labs", {
 
 	version = __DCS_VERSION__,
 	state = "installed",
-	info = _("The " .. self_ID .. " is a single-seat reconnaissance-bomber with revised ELINT equipment, the *Tangazh*, and a Peleng automatic bombing system. The aircraft can carry a maximum of ten 500 kg. (1,100 lb.) bombs. It is among the fastest military aircraft to enter service. Designed by the Soviet Union"s Mikoyan-Gurevich bureau, it is one of the few combat aircraft built primarily using stainless steel."),
+	info = _("The " .. self_ID .. " is a single-seat reconnaissance-bomber with " ..
+			 "revised ELINT equipment, the *Tangazh*, and a Peleng automatic " .. 
+			 "bombing system. The aircraft can carry a maximum of ten 500 kg. (1,100 lb.) " ..
+			 "bombs. It is among the fastest military aircraft to enter service. " .. 
+			 "Designed by the Soviet Union's Mikoyan-Gurevich bureau, it is one of " .. 
+			 "the few combat aircraft built primarily using stainless steel."),
 
 	InputProfiles = {[self_ID] = current_mod_path .. "/Input/" .. self_ID},
 
-	Skins = {{name = _(self_ID), dir = "Theme"}},
+	--- TODO: Implement themes.
+	-- Skins = {{name = _(self_ID), dir = "Theme"}},
 
 	--- TODO: Implement missions for training, campaigns and etc.
 	-- Missions ={{name = _(self_ID), dir = "Missions"}},		
@@ -46,11 +52,14 @@ declare_plugin(self_ID .. " Foxbat by Vapour Labs", {
 
 -- Mounts all the needed directory paths of the mod for DCS to interpret.
 local mount_vfs; do
-	mount_vfs_texture_path(themes_path)
+	--- TODO: Implement UI theme for main menu and loading assets.
+	-- mount_vfs_texture_path(themes_path)
 
 	--- TODO: Implement cockpit textures as compressed .ZIP file.
 	-- mount_vfs_texture_path(current_mod_path ..  "/Cockpit/Textures/MIG-25T-CPT-TEXTURES")
-	mount_vfs_model_path(model_path)
+
+	--- TODO: Implement 3D models in .EDM export format.
+	-- mount_vfs_model_path(model_path)
 
 	--- TODO: Implement liveries.
 	-- mount_vfs_liveries_path(liveries_path)
